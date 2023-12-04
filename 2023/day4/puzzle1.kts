ecaa2b548bbe9d75a1c7183705216c92334d5e53
Nicklas Ansman <nicklas@ansman.se>
2023-12-04 07:01:24 -0500

    Add day 4, puzzle 1

---
+++ 2023/day4/puzzle1.kts
@@ -0,0 +1,25 @@
+#!/usr/bin/env kotlin
+
+import kotlin.math.exp
+import kotlin.math.pow
+import kotlin.math.roundToInt
+
+val gamePattern = Regex("""Card\s+\d+:\s+""")
+val numberSplit = Regex("""\s+""")
+
+fun String.parseCard(): Int {
+    val (winning, played) = replace(gamePattern, "")
+            .split(" | ")
+            .map { it.trim() }
+            .map { numbers -> numbers.split(numberSplit).map { it.toInt() }.toSet() }
+    val winningNumbers = winning.intersect(played)
+            .takeUnless { it.isEmpty() }
+            ?.size
+            ?: return 0
+    return 2.0.pow(winningNumbers - 1).roundToInt()
+}
+
+generateSequence { readlnOrNull() }
+        .map { it.parseCard() }
+        .sum()
+        .let(::println)
