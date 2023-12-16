6e41b62019e601da1310b2d5ea5fc2a624e37113
Nicklas Ansman <nicklas@ansman.se>
2023-12-16 07:21:57 -0500

    Add day 16, puzzle 2

---
+++ 2023/day16/puzzle2.kts
@@ -0,0 +1,104 @@
+#!/usr/bin/env kotlin
+
+import Puzzle2.Beam.Direction.*
+import java.util.*
+import kotlin.math.*
+import kotlin.time.TimeSource
+
+val start = TimeSource.Monotonic.markNow()
+val debug = false
+
+val grid = generateSequence(::readlnOrNull).toList()
+
+data class Beam(val x: Int, val y: Int, val direction: Direction) {
+    val position = (x.toLong() shl 32) or y.toLong()
+
+    fun step(): Beam = copy(x = x + direction.dx, y = y + direction.dy)
+
+    fun reflect(mirror: Char): Beam {
+        val newDirection = when (mirror) {
+            '/' -> when (direction) {
+                Up -> Right
+                Down -> Left
+                Left -> Down
+                Right -> Up
+            }
+
+            '\\' -> when (direction) {
+                Up -> Left
+                Down -> Right
+                Left -> Up
+                Right -> Down
+            }
+
+            else -> error("Unknown mirror: $mirror")
+        }
+        return copy(direction = newDirection).step()
+    }
+
+    fun split(reflector: Char): List<Beam> {
+        val directions = when (direction) {
+            Left, Right -> {
+                when (reflector) {
+                    '-' -> listOf(direction)
+                    '|' -> listOf(Up, Down)
+                    else -> error("Unknown reflector: $reflector")
+                }
+            }
+
+            Up, Down -> {
+                when (reflector) {
+                    '-' -> listOf(Left, Right)
+                    '|' -> listOf(direction)
+                    else -> error("Unknown reflector: $reflector")
+                }
+            }
+        }
+        return directions.map { copy(direction = it).step() }
+    }
+
+    fun isInBounds(width: Int, height: Int): Boolean = x in 0 until width && y in 0 until height
+
+    enum class Direction(val dx: Int, val dy: Int) {
+        Up(0, -1),
+        Down(0, 1),
+        Left(-1, 0),
+        Right(1, 0);
+
+        val isVertical get() = this == Up || this == Down
+    }
+}
+
+fun energize(startingBeam: Beam): Int {
+    val beams = LinkedList(listOf(startingBeam))
+    val energized = mutableSetOf<Long>()
+    val seen = mutableSetOf<Beam>()
+    while (beams.isNotEmpty()) {
+        val beam = beams.removeFirst()
+        if (!beam.isInBounds(grid[0].length, grid.size)) {
+            continue
+        }
+        if (!seen.add(beam)) {
+            continue
+        }
+        energized.add(beam.position)
+        val (x, y) = beam
+        when (val c = grid[y][x]) {
+            '.' -> beams.add(beam.step())
+            '/', '\\' -> beams.add(beam.reflect(c))
+            '-', '|' -> beams.addAll(beam.split(c))
+            else -> error("Unknown character: $c")
+        }
+    }
+
+    return energized.size
+}
+
+emptySequence<Beam>()
+        .plus(grid[0].indices.asSequence().flatMap { sequenceOf(Beam(it, 0, Beam.Direction.Down), Beam(it, grid.lastIndex, Up)) })
+        .plus(grid.indices.asSequence().flatMap { sequenceOf(Beam(0, it, Beam.Direction.Right), Beam(grid[it].lastIndex, it, Beam.Direction.Left)) })
+        .map { energize(it) }
+        .max()
+        .let(::println)
+
+println(start.elapsedNow())
