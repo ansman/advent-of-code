0368ef28e910d4b560f48abcc99fbd0afe4284ed
Nicklas Ansman <nicklas@ansman.se>
2023-12-18 11:37:10 -0500

    Add puzzle 2

--- 2023/day18/puzzle2.kts
+++ 2023/day18/puzzle2.kts
@@ -14,44 +14,50 @@ enum class Direction(val dx: Int, val dy: Int) {
 }
 
 data class Instruction(
-        val direction: Direction,
-        val count: Int,
+    val direction: Direction,
+    val count: Int,
 ) {
     companion object {
         private val pattern = Regex("""[URDL] \d+ \(#([0-9a-fA-F]{6})\)""")
         fun parse(input: String): Instruction {
             val (color) = pattern.matchEntire(input)!!.destructured
             return Instruction(
-                    direction = Direction.entries[color.last().digitToInt()],
-                    count = color.dropLast(1).toInt(16)
+                direction = Direction.entries[color.last().digitToInt()],
+                count = color.dropLast(1).toInt(16)
             )
         }
     }
 }
 
 fun List<Pair<Long, Long>>.area(): Long {
-    var sum = 0L
+    var interiorArea = 0L
     for (i in indices) {
         val (x0, y0) = this[i]
         val (x1, y1) = this[(i + 1) % size]
-        sum += x0 * y1 - x1 * y0
+        interiorArea += (y0 + y1) * (x0 - x1)
     }
-    return sum.absoluteValue / 2
+    interiorArea = interiorArea.absoluteValue / 2
+
+    val border = asSequence()
+        .zipWithNext()
+        .fold(0L) { acc, (p0, p1) ->
+            acc + (p0.first - p1.first).absoluteValue + (p0.second - p1.second).absoluteValue
+        }
+
+    return interiorArea + border / 2 + 1
 }
 
-generateSequence(::readlnOrNull)
-        .map(Instruction::parse)
-        .onEach { println(it) }
-        .fold(mutableListOf(0L to 0L)) { acc, instruction ->
-            val (x, y) = acc.last()
-            val nx = x + instruction.direction.dx * instruction.count
-            val ny = y + instruction.direction.dy * instruction.count
-            acc.add(nx to ny)
-            acc
-        }
-        .onEach { println(it) }
-        .area()
-        .let(::println)
+val instructions = generateSequence(::readlnOrNull)
+    .map(Instruction::parse)
+    .fold(mutableListOf(0L to 0L)) { acc, instruction ->
+        val (x, y) = acc.last()
+        val nx = x + instruction.direction.dx * instruction.count
+        val ny = y + instruction.direction.dy * instruction.count
+        acc.add(nx to ny)
+        acc
+    }
+    .area()
+    .let(::println)
 
 
 println(start.elapsedNow())
